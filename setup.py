cf0f9c38e4249f6f543d28c825fe321d85207a1f
Christoffer Rehn <crehn@outlook.com>
2018-05-27 12:20:36 +0900

    Rename to kitten

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 import setuptools
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setuptools.setup(
-    name="damn",
+    name="kitten",
     version="0.0.1",
-    author="Christoffer Rehn",
+    author="Chris Rehn",
     author_email="chris@rehn.me",
+    description="Tiny tool to manage multiple servers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hoffa/kitten",
     packages=setuptools.find_packages(),
+    scripts=["bin/kitten"],
+    install_requires=["boto3>=1.7.0", "fabric>=2.1.0"],
     license="MIT",
-    install_requires=["boto3", "fabric"],
-    scripts=["bin/damn"],
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
