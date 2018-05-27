f828767f40d1ab773cfee73217255d11df3716e8
Christoffer Rehn <crehn@outlook.com>
2018-05-27 12:33:40 +0900

    Add pip install to Travis

--- setup.py
+++ setup.py
@@ -17,9 +17,13 @@ setuptools.setup(
     install_requires=["boto3>=1.7.0", "fabric>=2.1.0"],
     license="MIT",
     classifiers=(
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ),
 )
