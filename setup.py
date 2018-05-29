fc1d956e3d33dd3302db1edf103a56ad28b82c93
Christoffer Rehn <crehn@outlook.com>
2018-05-29 13:44:04 +0900

    Very lenient version requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/hoffa/kitten",
     packages=setuptools.find_packages(),
     entry_points={"console_scripts": {"kitten=kitten:main"}},
-    install_requires=["boto3", "fabric>=2.0", "six"],
+    install_requires=["boto3>=1.0", "fabric>=2.0", "six>=1.0"],
     license="MIT",
     classifiers=(
         "Development Status :: 4 - Beta",
