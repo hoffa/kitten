3f2cb9c9bbbea75251f0c346ee1bf692ee40d575
Christoffer Rehn <crehn@outlook.com>
2018-06-02 00:00:41 +0900

    Update version

--- kitten/__init__.py
+++ kitten/__init__.py
@@ -14,7 +14,7 @@ import boto3
 import fabric
 from six.moves import range, queue
 
-__version__ = "0.2.10"
+__version__ = "0.2.11"
 
 CHUNK_SIZE = 100
 DEFAULT = {"threads": 10, "timeout": 10}
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setuptools.setup(
     name="kitten",
-    version="0.2.10",
+    version="0.2.11",
     author="Chris Rehn",
     author_email="chris@rehn.me",
     description="Tiny multi-server automation tool.",
