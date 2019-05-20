1c68b4f7affbadabb8bb2c6298d164dd702ae8e2
Chris Rehn <crehn@outlook.com>
2019-05-20 15:15:27 +0900

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setuptools.setup(
     name="kitten",
-    version="0.5.0",
+    version="0.5.1",
     author="Chris Rehn",
     author_email="chris@rehn.me",
     description="Tiny multi-server automation tool.",
